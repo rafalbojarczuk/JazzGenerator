8464e2635991f8910e7afcf2c3332a7df80b3583
Rafał Bojarczuk <01130557@pw.edu.pl>
2020-03-18 00:29:37 +0100

    added script for creating lookup dictionaries

---
+++ create_dictionaries.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import music21
+import pickle
+
+
+dataset_dir = os.path.join(os.path.dirname(os.getcwd()), 'jazzdataset')
+
+notes_to_int = {"rest" : 0}
+durations = []
+offsets = []
+note_ind = 1
+for file in os.listdir(dataset_dir):
+	if file.endswith(".mid"):
+		print(file)
+		path = os.path.join(dataset_dir, file)
+		midi = music21.converter.parse(path)
+		parts = music21.instrument.partitionByInstrument(midi)
+		if parts:
+			notes_to_parse = parts.parts[0].recurse().notes
+		else:
+			notes_to_parse = midi.flat.notes
+		prev_offset = 0.0
+		for element in notes_to_parse:
+			if isinstance(element, music21.note.Note):
+				if element.nameWithOctave not in notes_to_int.keys():
+					notes_to_int[element.nameWithOctave] = note_ind
+					note_ind+=1
+			elif isinstance(element, music21.chord.Chord):
+				chord = '.'.join(note.nameWithOctave for note in element.pitches)
+				if chord not in notes_to_int.keys():
+					notes_to_int[chord] = note_ind
+					note_ind+=1
+			durations.append(element.duration.quarterLength)
+			#smallest offsets are 0.25 and 0.33 so I multiply by 12 and round
+			#it prevents adding different floating-point approximations to the dictionary
+			#i.e. 0.333312 and 0.33335 should be treated as the same value 1/3
+			offsets.append(round(12*(element.offset - prev_offset)))
+			prev_offset = element.offset
+durations = set(item for item in durations)
+offsets = set(item for item in offsets)
+duration_to_int = dict((duration, number) for number, duration in enumerate(durations))
+offset_to_int = dict((offset, number) for number, offset in enumerate(offsets))
+
+with open('notes_to_int', 'wb') as file:
+	pickle.dump(notes_to_int, file)
+with open('duration_to_int', 'wb') as file:
+	pickle.dump(duration_to_int, file)
+with open('offset_to_int', 'wb') as file:
+	pickle.dump(offset_to_int, file)
--- utils.py
+++ utils.py
@@ -9,71 +9,10 @@ import mido
 import py_midicsv
 import sys
 import music21
-
-
-def chord_to_one_hot(chord):
-	one_hot = np.zeros(shape=(96,1), dtype=np.int32)
-	base_note = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-	for note in chord.pitches:
-		base_note_index = base_note.index(note.name[0])
-		note_index = base_note_index + 12*note.octave
-		if '#' in note.name:
-			note_index+=1
-		elif '-' in note.name:
-			note_index-=1
-		one_hot[note_index] = 1
-
-	return one_hot
-
-def midi_to_matrix(midi):
-	matrix = np.empty(shape=(98,1), dtype=np.int32)
-	prev_offset = 0.0
-	for chord in midi.flat.notes:
-		one_hot_list = chord_to_one_hot(chord)
-		#smallest duration times/offsets are 0.25 and 0.33 so I multiply by 12 to convert them all to integer
-		one_hot_list = np.append(one_hot_list, round(chord.duration.quarterLength*12))
-		one_hot_list = np.append(one_hot_list, round(12*((chord.offset) - prev_offset)))
-		prev_offset = float(chord.offset)
-		matrix = np.hstack([matrix, one_hot_list.reshape(98,1)])
-	matrix = np.delete(matrix, obj=0, axis=1)
-	print(matrix.shape)
-	return matrix
-
-def create_lookup_dictionaries(directory):
-	notes_to_int = dict()
-	duration_to_int = dict()
-	offset_to_int = dict()
-	note_ind = 0
-	dur_ind = 0
-	offset_ind = 0
-	for file in os.listdir(directory)[-16:]:
-		if file.endswith(".mid"):
-			print(file)
-			path = os.path.join(dataset_dir, file)
-			midi = music21.converter.parse(path)
-			if music21.instrument.partitionByInstrument(midi):
-				print("instruments")
-			for element in midi.flat.notes:
-				if isinstance(element, music21.note.Note):
-					if element.nameWithOctave not in notes_to_int.keys():
-						notes_to_int[element.nameWithOctave] = note_ind
-						note_ind+=1
-				elif isinstance(element, music21.chord.Chord):
-					chord = ""
-					for note in element.pitches:
-						chord += note.nameWithOctave
-					if chord not in notes_to_int.keys():
-						notes_to_int[chord] = note_ind
-						note_ind+=1
-	return notes_to_int
+import pickle
 
 
 
 dataset_dir = os.path.join(os.path.dirname(os.getcwd()), 'jazzdataset')
-print(create_lookup_dictionaries(dataset_dir))
-#for file in os.listdir(dataset_dir)[:1]:
-#	if file.endswith(".mid"):
-#		path = os.path.join(dataset_dir, file)
-	#	midi = music21.converter.parse(path)
-	#	print(midi_to_matrix(midi)[90:,-5:].T)
+
 		
